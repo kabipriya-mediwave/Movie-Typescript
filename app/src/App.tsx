f242d2b3a95cc8ef4da0138c21f27de8bb63a4d2
kabipriya-mediwave <kabipriya@mindwaveventures.com>
2023-10-30 14:48:53 +0530

    added network

---
+++ app/src/App.tsx
@@ -0,0 +1,30 @@
+import "@picocss/pico";
+import { Suspense, lazy, useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import { IMovie } from "./type";
+
+
+const Add = lazy(() => import("./pages/Add"));
+const Edit = lazy(() => import("./pages/Edit"));
+
+function App() {
+  const [movie, setMovie] = useState<IMovie>({
+    id: 0,
+    title: "",
+    year: 0,
+  });
+  return (
+    <Suspense>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home handleEdit={(m) => setMovie(m)} />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/edit/:id" element={<Edit movie={movie} />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
+  );
+}
+
+export default App;
